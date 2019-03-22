239996db03834530f1b2aad22509b22ab6707a95
Argg1025 <agoddard@spartaglobal.com>
2019-03-22 16:07:51 +0000

    First commit

---
+++ NorthWindTests/NorthwindService/DataHandler/NorthwindDTO.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace NorthWindTests
+{
+    public class NorthwindDTO
+    {
+        public List<Customer> customers = new List<Customer>();
+        public Customer firstCustomer = new Customer();
+
+         public Customer FirstCustomerToObject()
+        {
+            using (var db = new NorthwindEntities())
+            {
+                firstCustomer = db.Customers.FirstOrDefault();
+            }
+            return firstCustomer;
+        }
+        public List<Customer> CustomerToObject()
+        {
+            using (var db = new NorthwindEntities())
+            {
+                customers = db.Customers.ToList<Customer>();
+            }
+            return customers;
+        }
+
+        public int IDLength(string id)
+        {
+            return id.Length;
+        }
+
+        public bool UniqueID()
+        {
+            List<string> customerIDS = new List<string>();
+            foreach (var customer in customers)
+            {
+                customerIDS.Add(customer.CustomerID);
+            }
+            if (customerIDS.Count == customerIDS.Distinct().Count()) return true;
+            else return false;
+        }
+    }
+}
---
+++ NorthWindTests/Tests/UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NorthWindTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        NorthwindDTO northwindDTO = new NorthwindDTO();
+
+        [Test]
+        public void DistinctIDs()
+        {
+            Assert.AreEqual(true, northwindDTO.UniqueID());
+        }
+
+        [Test]
+        public void TestID()
+        {
+            Assert.AreEqual(5, northwindDTO.IDLength(northwindDTO.CustomerToObject()[3].CustomerID));
+            Assert.AreEqual("ADAM5", northwindDTO.CustomerToObject()[3].CustomerID);
+        }
+
+        [Test]
+        public void TestName()
+        {
+            Assert.AreEqual("Fred Flintstone", northwindDTO.CustomerToObject()[3].ContactName);
+        }
+
+        [Test]
+        public void TestCompanyName()
+        {
+            Assert.AreEqual("Alfreds Futterkiste", northwindDTO.CustomerToObject()[3].CompanyName);
+        }
+
+        [Test]
+        public void TestContactTitle()
+        {
+            Assert.AreEqual("Sales Representative", northwindDTO.CustomerToObject()[3].ContactTitle);
+        }
+
+        [Test]
+        public void TestAddress()
+        {
+            Assert.AreEqual("Obere Str. 57", northwindDTO.CustomerToObject()[3].Address);
+        }
+
+        [Test]
+        public void TestCity()
+        {
+            Assert.AreEqual("Bed Rock", northwindDTO.CustomerToObject()[3].City);
+        }
+
+        [Test]
+        public void TestRegion()
+        {
+            Assert.AreEqual(null, northwindDTO.CustomerToObject()[3].Region);
+        }
+
+        [Test]
+        public void TestPostalcode()
+        {
+            Assert.AreEqual("12209", northwindDTO.CustomerToObject()[3].PostalCode);
+        }
+
+        [Test]
+        public void TestCountry()
+        {
+            Assert.AreEqual("Germany", northwindDTO.CustomerToObject()[3].Country);
+        }
+
+        [Test]
+        public void TestPhone()
+        {
+            Assert.AreEqual("030-0074321", northwindDTO.CustomerToObject()[3].Phone);
+        }
+
+        [Test]
+        public void TestFax()
+        {
+            Assert.AreEqual("030-0076545", northwindDTO.CustomerToObject()[3].Fax);
+        }
+    }
+}
